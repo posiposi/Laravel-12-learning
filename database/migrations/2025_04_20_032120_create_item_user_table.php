af74a4f00442c9eab72699116d47f1c9cf280566
daichi.sugiyama <caad12.di2@gmail.com>
2025-04-20 12:55:49 +0900

    #6 中間テーブル追加

---
+++ database/migrations/2025_04_20_032120_create_item_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_user', function (Blueprint $table) {
+            $table->ulid('item_id')->comment('商品ID');
+            $table->ulid('user_id')->comment('ユーザーID');
+            $table->primary(['item_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_user');
+    }
+};
